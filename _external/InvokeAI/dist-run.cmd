5e8df430e7a70f6b2ee0d2619be99e19a4fdaa21
Yaroslav Sivakov <yar3333@gmail.com>
2022-10-24 13:40:21 +0300

    * move big stuff to common directory

--- _external/InvokeAI/dist-run.cmd
+++ _external/InvokeAI/dist-run.cmd
@@ -1,7 +1,7 @@
 @pushd dist
 
 @setlocal
-set PATH=%~dp0\stuff\NVIDIA GPU Computing Toolkit_CUDA_v11.4\bin;%PATH%
+set PATH=%~dp0..\_stuff\NVIDIA GPU Computing Toolkit_CUDA_v11.4\bin;%PATH%
 set TORCH_HOME=%~dp0dist\stuff\models
 set XDG_CACHE_HOME=%~dp0dist\stuff\models
 legacy_api\aipainter_invokeai.exe --web %*
--- _external/rembg/dist-run.cmd
+++ _external/rembg/dist-run.cmd
@@ -1,7 +1,7 @@
 @pushd dist
 
-set PATH=%~dp0TensorRT-8.4.2.4\lib;%PATH%
-set PATH=%~dp0cudnn-windows-x86_64-8.4.1.50_cuda11.6\bin;%PATH%
+set PATH=%~dp0..\_stuff\TensorRT-8.4.2.4\lib;%PATH%
+set PATH=%~dp0..\_stuff\cudnn-windows-x86_64-8.4.1.50_cuda11.6\bin;%PATH%
 aipainter_rembg\aipainter_rembg.exe s --port 9696
 
 @popd
