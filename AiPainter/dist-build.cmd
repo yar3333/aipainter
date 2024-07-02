0e6fb42f399ba30bcfac9f22fc357cb54dfcd2d5
Yaroslav Sivakov <yar3333@gmail.com>
2024-07-02 14:51:36 +0300

    * minor

--- AiPainter/dist-build.cmd
+++ AiPainter/dist-build.cmd
@@ -5,7 +5,7 @@ dotnet publish -p:PublishSingleFile=true /p:DebugType=None -r win-x64 -c Release
 
 if not exist dist\AiPainter\external                 mkdir dist\AiPainter\external
 if not exist dist\AiPainter\external\_stuff          mklink /D dist\AiPainter\external\_stuff %~dp0..\_external\_stuff 2> nul
-if not exist dist\AiPainter\external\StableDiffusion mklink /D dist\AiPainter\external\StableDiffusion %~dp0..\_external\StableDiffusion\repo 2> nul
+if not exist dist\AiPainter\external\StableDiffusion mklink /D dist\AiPainter\external\StableDiffusion %~dp0..\_external\StableDiffusion 2> nul
 if not exist dist\AiPainter\external\lama-cleaner    mklink /D dist\AiPainter\external\lama-cleaner %~dp0..\_external\lama-cleaner\dist 2> nul
 if not exist dist\AiPainter\external\rembg           mklink /D dist\AiPainter\external\rembg %~dp0..\_external\rembg\dist 2> nul
 
