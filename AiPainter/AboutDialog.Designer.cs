7aa7afc38f107dc693b7b2c2b56d60531ce136c0
Yaroslav Sivakov <yar3333@gmail.com>
2024-07-06 13:30:26 +0300

    * "about" dialog fix

--- AiPainter/AboutDialog.Designer.cs
+++ AiPainter/AboutDialog.Designer.cs
@@ -28,53 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btOk = new System.Windows.Forms.Button();
-            this.rtbText = new System.Windows.Forms.RichTextBox();
-            this.SuspendLayout();
+            btOk = new Button();
+            rtbText = new RichTextBox();
+            SuspendLayout();
             // 
             // btOk
             // 
-            this.btOk.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btOk.Location = new System.Drawing.Point(218, 393);
-            this.btOk.Name = "btOk";
-            this.btOk.Size = new System.Drawing.Size(260, 45);
-            this.btOk.TabIndex = 0;
-            this.btOk.Text = "OK";
-            this.btOk.UseVisualStyleBackColor = true;
-            this.btOk.Click += new System.EventHandler(this.btOk_Click);
+            btOk.Anchor = AnchorStyles.Bottom;
+            btOk.Location = new Point(218, 360);
+            btOk.Name = "btOk";
+            btOk.Size = new Size(260, 45);
+            btOk.TabIndex = 0;
+            btOk.Text = "OK";
+            btOk.UseVisualStyleBackColor = true;
+            btOk.Click += btOk_Click;
             // 
             // rtbText
             // 
-            this.rtbText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.rtbText.BackColor = System.Drawing.SystemColors.Control;
-            this.rtbText.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.rtbText.Location = new System.Drawing.Point(12, 12);
-            this.rtbText.Name = "rtbText";
-            this.rtbText.Size = new System.Drawing.Size(663, 369);
-            this.rtbText.TabIndex = 1;
-            this.rtbText.Text = "About text";
-            this.rtbText.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.rtbText_LinkClicked);
+            rtbText.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            rtbText.BackColor = SystemColors.Control;
+            rtbText.BorderStyle = BorderStyle.None;
+            rtbText.Location = new Point(12, 12);
+            rtbText.Name = "rtbText";
+            rtbText.Size = new Size(663, 324);
+            rtbText.TabIndex = 1;
+            rtbText.Text = "About text";
+            rtbText.LinkClicked += rtbText_LinkClicked;
             // 
             // AboutDialog
             // 
-            this.AcceptButton = this.btOk;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 450);
-            this.Controls.Add(this.rtbText);
-            this.Controls.Add(this.btOk);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "AboutDialog";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "About application";
-            this.ResumeLayout(false);
-
+            AcceptButton = btOk;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(687, 427);
+            Controls.Add(rtbText);
+            Controls.Add(btOk);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "AboutDialog";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "About application";
+            ResumeLayout(false);
         }
 
         #endregion
